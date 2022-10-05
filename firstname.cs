856ad3c6fe73360c1ea28c140e9e46a07b7c0106
ams162001 <101324094+ams162001@users.noreply.github.com>
2022-10-05 15:38:40 +0530

    Add files via upload

---
+++ firstname.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+        public static void Display()
+        {
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("First Name " + firstName.ToUpper());
+
+            Console.WriteLine("Last Name " + lastName.ToUpper());
+            Console.ReadLine();
+        }
+
+    }
+}
