6f615745a1cbcb5b7028d834dd131f65093fcd3a
ams162001 <101324094+ams162001@users.noreply.github.com>
2022-10-09 23:06:51 +0530

    Add files via upload

---
+++ A5.3b.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary2;
+using System;
+
+
+namespace Assignment_5
+{
+    class Program3b
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Concession concession = new Concession();      //using user defined "USING DERECTIVE REFERENCE."
+            concession.CalculateConcession(age);
+            Console.ReadLine();
+
+        }
+    }
+}
