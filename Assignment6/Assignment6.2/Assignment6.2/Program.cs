7ba7aa4dde8374be739c4f556b6af7c63fac2abd
ams162001 <101324094+ams162001@users.noreply.github.com>
2022-10-15 22:03:06 +0530

    Add files via upload

---
+++ Assignment6/Assignment6.2/Assignment6.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ASSIGNMENT_6_
+{
+    class TESTSTRUCTURES
+    {
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            books.SetValues("ROMEO AND JULIET", "Atman Ravi", "LOVE AND LIFE", 1030061);
+            books.GetValues();
+            Console.Read();
+        }
+    }
+    struct Books
+    {
+        private string title; private string Author; private string Subject; private int Book_Id;
+
+        public void GetValues()
+        {
+            Console.WriteLine("Book Details");
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + Author);
+            Console.WriteLine("Subject: " + Subject);
+            Console.WriteLine("Book ID: " + Book_Id);
+
+        }
+        public void SetValues(string Title, string author, string subject, int BId)
+        {
+            title = Title; Author = author; Subject = subject; Book_Id = BId;
+        }
+
+    }
+
+}
---
+++ Assignment6/Assignment6.3/Assignment6.3/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ASSIGNMENT_6_
+{
+    public class PRODUCTS
+    {
+        static void Main(string[] args)
+        {
+            List<Products> stdlist = new List<Products>
+            {
+                new Products{ProductId=1,Product_Name="SOAP",Price=100.0f},
+                new Products{ProductId=2,Product_Name="BRUSH",Price=200.0f},
+                new Products{ProductId=3,Product_Name="WASHINGMACHINE",Price=300000.0f},
+                new Products{ProductId=4,Product_Name="LAPTOP",Price=400000.0f},
+                new Products{ProductId=5,Product_Name="MOBILEPHONE",Price=500000.0f},
+                new Products{ProductId=6,Product_Name="AIRCONDITIONER",Price=600000.0f},
+                new Products{ProductId=7,Product_Name="FAN",Price=7000.0f},
+                new Products{ProductId=8,Product_Name="WEDGES",Price=800.0f},
+                new Products{ProductId=9,Product_Name="BACKPACKS",Price=900.0f},
+                new Products{ProductId=10,Product_Name="DININGTABLE",Price=10000.0f}
+            };
+            Display(stdlist);
+            Console.Read();
+        }
+        public static void Display(List<Products> products)
+        {
+            Console.WriteLine();
+            var val = products.OrderBy(a => a.Price).ToList();
+            foreach (var p in val)
+            {
+                Console.WriteLine("name: {0}   id: {1}   Price: {2} ", p.Product_Name, p.ProductId, p.Price);
+            }
+        }
+    }
+
+    public class Products
+    {
+        public int ProductId
+        {
+            get;
+            set;
+        }
+        public string Product_Name
+        {
+            get;
+            set;
+        }
+        public float Price
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
