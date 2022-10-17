013bf9a1ec3b9e391504566d8a6c7c47d152bdbb
ams162001 <101324094+ams162001@users.noreply.github.com>
2022-10-17 10:28:38 +0530

    Add files via upload

---
+++ program1.match/program1.match/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Program
+{
+    class Cricket
+    {
+        static void no_of_matches(int Total) 
+        { 
+            float Average; int Sum = 0, Value; for (int i = 0; i < Total; i++) 
+            { 
+                Console.WriteLine("Enter a total runs in  matches : "); 
+                Value = Convert.ToInt32(Console.ReadLine()); 
+                Sum += Value; 
+            }
+            Average = (float)Sum / Total; 
+            Console.WriteLine("Sum: " + Sum + ", Average: " + Average); 
+        }
+        static void Main(string[] args)
+        {
+            int Total; Console.WriteLine("Enter the total count of numbers: ");
+            Total = Convert.ToInt32(Console.ReadLine());
+            no_of_matches(Total);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
