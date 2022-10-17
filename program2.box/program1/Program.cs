ca5fc92522e30adca33e94b9942eb3385199dff0
ams162001 <101324094+ams162001@users.noreply.github.com>
2022-10-17 10:27:36 +0530

    Add files via upload

---
+++ program2.box/program1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+
+    class Box
+    {
+        // member variables
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 10.5;
+            width = 20.5;
+        }
+
+        public double GetAddition()
+        {
+            return length + width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Addition: {0}", GetAddition());
+        }
+    }
+
+    class ExecuteBox
+    {
+
+        static void Main(string[] args)
+        {
+            Box r = new Box();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
+
